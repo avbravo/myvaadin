bf9dea4e02e5a32b164a76c0fc8e1d12180fd79c
avbravo <avbravo@avbravo-ThinkCentre-M70t-Gen-3>
2024-08-15 15:00:43 -0500

    Vaadin

---
+++ src/main/java/com/avbravo/myvaadin/MainView.java
@@ -0,0 +1,74 @@
+package com.avbravo.myvaadin;
+
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.formlayout.FormLayout.ResponsiveStep;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.Scroller;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.sidenav.SideNav;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+
+@Route
+public class MainView extends VerticalLayout {
+ String password ="";
+          String username ="";
+    public MainView() {
+
+        addClassName("login-rich-content");
+
+        LoginForm loginForm = new LoginForm();
+        loginForm.getElement().getThemeList().add("dark");
+        add(loginForm);
+        
+        loginForm.addLoginListener(e -> {
+          password = e.getPassword();
+       username = e.getUsername();
+   System.out.println("\t username "+username + " passwrord ");
+        });
+
+        System.out.println("\t username "+username + " passwrord ");
+        Button button = new Button("Click me",
+                event -> add(new Paragraph("Clicked!")));
+
+        add(button);
+
+        TextField firstName = new TextField("First name");
+        TextField lastName = new TextField("Last name");
+        TextField username = new TextField("Username");
+        PasswordField password = new PasswordField("Password");
+        PasswordField confirmPassword = new PasswordField("Confirm password");
+
+        FormLayout formLayout = new FormLayout();
+        formLayout.add(firstName, lastName, username, password,
+                confirmPassword);
+        formLayout.setResponsiveSteps(
+                // Use one column by default
+                new ResponsiveStep("0", 1),
+                // Use two columns, if layout's width exceeds 500px
+                new ResponsiveStep("500px", 2));
+// Stretch the username field over 2 columns
+        formLayout.setColspan(username, 2);
+        add(formLayout);
+
+        // Create a listing component for a bean type
+        Grid<Person> grid = new Grid<>(Person.class);
+
+// Sets items using vararg beans
+        grid.setItems(
+                new Person("George Washington", 1732),
+                new Person("John Adams", 1735),
+                new Person("Thomas Jefferson", 1743),
+                new Person("James Madison", 1751)
+        );
+        add(grid);
+
+    }
+}
---
+++ src/main/java/com/avbravo/myvaadin/Person.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.avbravo.myvaadin;
+
+/**
+ *
+ * @author avbravo
+ */
+public class Person {
+    private String name;
+    private Integer age;
+
+    public Person() {
+    }
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
